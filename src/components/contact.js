6fd8657b50f3a2a058e2c2b3da8828ec7f32297c
Peter Davis <petersdavis@gmail.com>
2022-09-24 17:01:02 -0400

    added lawn sign form

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,6 +11,7 @@ import FacebookLogo from '../images/facebook.png';
 import InstagramLogo from '../images/instagram.png';
 import GmailLogo from '../images/gmail.png'
 import TwitterLogo from '../images/twitter.png'
+import lawnSign from '../images/lsign.png'
 
 
 export default class extends React.Component {
@@ -20,15 +21,39 @@ export default class extends React.Component {
             email:"",
             feedback:"",
             consent:"Okay",
-            success:false        
+            success:false, 
+            
+            l_requested: false,
+            l_name: "",
+            l_address: "",
+            l_special:"",
         }
 
         this.setEmail = this.setEmail.bind(this);
         this.setFeedback = this.setFeedback.bind(this);
-
         this.setConsent = this.setConsent.bind(this);
-
         this.setSuccess = this.setSuccess.bind(this);
+        this.setLAddress = this.setLAddress.bind(this);
+        this.setLRequested = this.setLRequested.bind(this);
+        this.setLSpecial = this.setLSpecial.bind(this);
+
+    }
+
+    setLRequested(l_requested){
+        this.setState({l_requested});
+    }
+
+    setLAddress(l_address){
+        this.setState({l_address});
+    }
+
+   
+    setLName(l_name){
+        this.setState({l_name});
+    }
+
+    setLSpecial(l_special){
+        this.setState({l_special});
 
     }
 
@@ -42,6 +67,23 @@ export default class extends React.Component {
         this.setState({consent})
     }
     setSuccess(success){
+        if(success=== undefined) {
+            this.setState({
+                email:"",
+                feedback:"",
+                consent:"Okay",
+                success:false, 
+                
+                l_requested: false,
+                l_name: "",
+                l_address: "",
+                l_special:"",
+            }
+        )
+
+            return
+        }
+
         this.setState({success})
     }
     render(){
@@ -52,16 +94,45 @@ export default class extends React.Component {
         this.setSuccess(true);
     }   
 
+    const {l_requested, l_address, l_name, l_special} = this.state;
+
+
     return (
         <div ref = {ref} className={"contact-us section"}>
+            <div className={'contents contact-form'}>
+                <img className="lawnsign-img" src={lawnSign}></img>
+                <h1 className="section-title lawnsign-title"> Lawn Signs Request: </h1>
+                <img className="lawnsign-img" src={lawnSign}></img>
+
+                <label className="check-container"><h2>Sign me up!</h2> <input className="checkbox" type={"checkbox"} checked={l_requested} onChange={event=>this.setLRequested(!l_requested)}/><span className={'checkmark'}></span> </label>
+                {
+                    l_requested === true ? 
+                    <div className = "lawn-sign-details">
+                        <form  target ={"_blank"} action={"https://docs.google.com/forms/u/1/d/e/1FAIpQLSfHtDAjSua-qaIm3OF6YGODaQGxYJTmz2EUqs2lIhnUAbrP6g/formResponse"}>
+
+                        <label> <span>Household Name:</span><input name={"entry.1727604807"} placeholder={"Name"} value={l_name} onChange={event=>this.setLName(event.target.value)}/></label>
+                        <label> <span>Address:</span><input name={"entry.1194664123"} placeholder={"Address"} value={l_address} onChange={event=>this.setLAddress(event.target.value)}/></label>
+                        <label> <span>Special Instructions:</span><textarea name={"entry.2027983411"} placeholder={"Special Instructions"} type={"text"} lines={"3"} value={l_special} onChange={event=>this.setLSpecial(event.target.value)}/></label>
+
+                        <button className={'submit-button'} onClick={submit}>Submit</button>
+                        </form>
+                    </div> :
+                    null
+                }
+
+            </div>
             
-            <form  className={"contents"} target ={"_blank"} action={"https://docs.google.com/forms/u/1/d/e/1FAIpQLScugQZPQRoZYU-xugtSYumCkDBgnBgqIXZodepknKx03tsrhw/formResponse"}>
+            
+        
+
+            
+            <form  className={"contents contact-form"} target ={"_blank"} action={"https://docs.google.com/forms/u/1/d/e/1FAIpQLScugQZPQRoZYU-xugtSYumCkDBgnBgqIXZodepknKx03tsrhw/formResponse"}>
                 <h1 className="section-title"> Contact Us: </h1>
                 <hr/>
                 <div className = {"col"}>
 
                     <p>Email Address: </p>
-                    <input value={email} placeholder={"email"} name={"entry.268754171"} onChange={event => this.setEmail(event.target.value)}></input>
+                    <input value={email} placeholder={"email"} name={"entry.268754171"} onChange={event => this.setEmail(event.target.value)} />
                     <p>Feedback:</p>
                     <textarea  value={feedback} rows = {3} placeholder={"feedback"} name={"entry.1364789300"} onChange={event => this.setFeedback(event.target.value)}></textarea>
                     <p>Would you like us to contact you?</p>
