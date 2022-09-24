af3422ac988cf9b12b2d084e2824dd65fc676ffa
Peter Davis <petersdavis@gmail.com>
2022-09-24 17:23:53 -0400

    added indigenous relations piece

--- src/components/platform.js
+++ src/components/platform.js
@@ -5,6 +5,7 @@ import housingImage from '../images/housing.jpg';
 import climateImage from '../images/climate.png';
 import financeImage from '../images/finance.png';
 import rentImage from '../images/rentPrices.png';
+import indigenousImage from '../images/indigenous.png'
 
 export default class extends React.Component {
 
@@ -19,8 +20,8 @@ export default class extends React.Component {
                 <hr/> 
                 <h1 className="sub-title">Affordable Housing: </h1>
                 <img className="section-image" src={housingImage}/>
-                <p>Our city is growing rapidly. There is no wonder why - people move to Kitchener for our peaceful lifestyle and for the many opportunities in the local technology sector. However, increased demand for housing has pushed rental prices up–in the last 5 years, <a href={"https://www.zumper.com/rent-research/kitchener-on"}>the cost of a 1-bedroom apartment has doubled!</a></p>
-                <p>As it stands, working individuals and families are at risk of homelessness due to lack of affordable housing. Furthermore, as our neighbours spend more and more of their household budgets on housing, they have less money to experience all that Kitchener’s businesses have to offer. In this housing climate, everybody suffers.</p>
+                <p>Council has a responsibility to ensure that as the city grows there is enough housing for all residents.  Over the last five years demand for housing has pushed rental prices up, <a href={"https://www.zumper.com/rent-research/kitchener-on"}>the cost of a 1-bedroom apartment has doubled!</a></p>
+                <p>Working families and individuals on fixed income are at a risk of homelessness if they lose their current accomodation.</p>
                 <p>Together with a progressive plan to increase the supply of housing in the city, we can ensure that there is enough housing for the whole community.</p>
                 <a target="_blank" href={"https://www.zumper.com/rent-research/kitchener-on"}>
                     <img className="rent-image" src={rentImage}/>
@@ -66,22 +67,20 @@ export default class extends React.Component {
                         <p>As Canada moves toward a low-carbon economy, we can work with Kitchener-Wilmot Hydro to make sure that we are ahead of the curve as Canada. In the long-run, an abundance of sustainable energy will make our community more competitive and a less expensive place to live.</p>
                     </li>
                 </ul>
-
-                <h1 className="sub-title">Financial Responsibility: </h1>
+            </div>
+            <div className = "contents">
+                <h1 className="sub-title">Fiscal Responsibility: </h1>
                 <img className="section-image" src={financeImage}/>
                 <p>Global supply chain issues, the war in Ukraine, and the lingering pandemic are having a profound impact on our economy.  Businesses still recovering from Covid-19 lockdowns are now struggling with inflation nearing 8%.  During this period of high inflation, we need our municipal government to exercise fiscal restraint.  Municipal council can’t change the price of canola oil - but when we choose to launch major infrastructure projects like the 93$ million recreational facility in Schlegel Park, we are putting the city in direct competition with other local developments for scarce local resources.  This kind of development is likely to increase costs across the region making it even harder for Kitchener residents to achieve their financial goals.</p>
-                <h1 className="sub-title">Our Plan: </h1>
-                <ul>
-                    <li>
-                        <h2>House the homeless:</h2>
-                        <p>Solving homelessness in the region would yield immediate financial benefit for our community.  We need an immediate investment from the region in safe, secure emergency shelters for the people living in tent-cities on Victoria St. and in Victoria Park.</p>
-                        <p>We also need an adequate supply of affordable and supportive housing so that people transitioning out of homelessness are not placed into inappropriate and unsustainable market rent accommodations.  By both investing in supportive and affordable housing and increasing the supply of market housing, it is possible that everyone can have a place to live in our city.</p>
-                    </li>
-                    <li>
-                        <h2>Protect our green space and trails:</h2>
-                        <p>We can intensify our downtown neighbourhoods without sacrificing their unique character and livability.  As a city, we should never sell our assets to subsidise short-term spending - the green spaces and trails that we have in the downtown core must be protected.</p>
-                    </li>
-                </ul>
+            </div>
+
+            <div className = "contents">
+                <h1 className="sub-title">Indigenous Relations: </h1>
+                <img className="section-image" src={indigenousImage}/>
+                <p>Peter is optimistic about the future of indigenous/non-indigenous relations in our community and our country.  The Truth and Reconciliation Commission began a process which continues today whereby all Canadians can begin to have a shared understanding of our past.</p>
+                <p>While there is value in truth, it’s important to recognize that truth in and of itself is not reconciliation.  The prerequisite for reconciliation is for non-indigenous Canadians to recognize that its in the interest of all Canadians to promote strong indigenous communities in our midst. </p>
+                <p>Peter is prepared to collaborate with indigenous representatives to empower them to achieve their vision of a permanent space for indigenous life and culture in the Region of Waterloo.</p>
+                <p>As a gesture of Peter’s intention to listen to and to collaborate with indigenous leaders, our campaign has made a small financial contribution to the <a href={"https://aocan.org/"}>Anishinabeg Outreach Center</a> located in Kitchener.</p>
             </div>
         </div>
     )
